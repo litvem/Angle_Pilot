0826f5a86150f7c8922e3a221b3b2e7c34607ebb
Bao Quan Lindgren <guslindgba@student.gu.se>
2023-03-30 12:14:58 +0200

    Upload example code

---
+++ example/PrimeChecker.cpp
@@ -0,0 +1,21 @@
+#include "PrimeChecker.hpp"
+
+bool PrimeChecker::isPrime(uint16_t n)
+{
+    if (n < 2 || n % 2 == 0)
+    {
+        return false;
+    }
+    else
+    {
+        for (uint16_t i{3}; i * i <= n; i += 2)
+        {
+            if (n % i == 0)
+            {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
---
+++ example/TestPrimeChecker.cpp
@@ -0,0 +1,11 @@
+#define CATCH_CONFIG_MAIN
+
+#include "catch.hpp"
+#include "PrimeChecker.hpp"
+
+TEST_CASE("Test PrimeChecker 1")
+{
+    PrimeChecker pc;
+    REQUIRE(pc.isPrime(5));
+}
+

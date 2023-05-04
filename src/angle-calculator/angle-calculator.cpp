41a78bba5df4ff441b3bcb20df74711c2e07fbbd
Bao Quan Lindgren <guslindgba@student.gu.se>
2023-05-04 19:22:59 +0200

    #13 Change float_t vars to _Float32 vars

--- src/angle-calculator/angle-calculator.cpp
+++ src/angle-calculator/angle-calculator.cpp
@@ -18,8 +18,8 @@
  * @param constant the constant of the linear function
  */
 struct line_t {
-    float_t coefficient;
-    float_t constant;
+    _Float32 coefficient;
+    _Float32 constant;
 };
 
 /**
@@ -30,8 +30,8 @@ struct line_t {
  * @param y the y coordinate of the point
  */
 struct point_t {
-    float_t x;
-    float_t y;
+    _Float32 x;
+    _Float32 y;
 };
 
 void handleExit(int sig);
@@ -49,9 +49,9 @@ line_t getLineFromCones(pos_api::cone_t close, pos_api::cone_t far);
 
 point_t getIntersect(line_t f, line_t g);
 
-float_t getAngle(point_t origin, point_t p);
+_Float32 getAngle(point_t origin, point_t p);
 
-float_t calculateSteering(pos_api::data_t data);
+_Float32 calculateSteering(pos_api::data_t data);
 
 // Main entry point
 int32_t main(int32_t argc, char **argv)
@@ -102,9 +102,9 @@ void handleExit(int sig)
 line_t getLineFromCones(pos_api::cone_t close, pos_api::cone_t far)
 {
     // dy/dx
-    float_t coeff = (far.posY - close.posY) / (far.posX - close.posX);
+    _Float32 coeff = (far.posY - close.posY) / (far.posX - close.posX);
     // y1 - ax1
-    float_t constant = far.posY - (coeff * far.posX);
+    _Float32 constant = far.posY - (coeff * far.posX);
 
     return {coeff, constant};
 }

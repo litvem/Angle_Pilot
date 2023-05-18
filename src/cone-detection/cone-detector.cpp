e8c9ea478d6a2808499e6a22f615ff56d60bd887
Robert <guseinero@student.gu.se>
2023-05-18 11:26:47 +0200

    #15 Put old license back in cone-detector.cpp

--- src/cone-detection/cone-detector.cpp
+++ src/cone-detection/cone-detector.cpp
@@ -1,19 +1,18 @@
 /*
-* AnglePilot - A system that determines steering angles for miniature cars
+* Copyright (C) 2020  Christian Berger
 * Copyright (C) 2023  Robert Einer, Emma Litvin, Ossian Ålund, Bao Quan Lindgren, Khaled Adel Saleh Mohammed Al-Baadani
-*
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
 * the Free Software Foundation, either version 3 of the License, or
 * (at your option) any later version.
-*
+* 
 * This program is distributed in the hope that it will be useful,
 * but WITHOUT ANY WARRANTY; without even the implied warranty of
 * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 * GNU General Public License for more details.
-* 
+*
 * You should have received a copy of the GNU General Public License
-* along with this program.  If not, see https://www.gnu.org/licenses/. 
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 //include section
